98bbfc2948d5cefb73c8e004d68a8ff01418066f
Peterpce <peterpec2022@gmail.com>
2026-05-26 18:53:44 +0100

    check the one

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src", "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
   res.render("index", {
